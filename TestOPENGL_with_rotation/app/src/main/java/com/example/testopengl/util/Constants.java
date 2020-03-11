0128784e76f61bea57613b1b9982e4ec22c79fd2
scheninsp <songchen7788@163.com>
2020-03-12 05:38:31 +0800

    1
    1

--- TestOPENGL_with_rotation/app/src/main/java/com/example/testopengl/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.example.testopengl.util;
-
-public class Constants {
-    public static final int BYTES_PER_FLOAT = 4;
-}
--- TestOPENGL_with_rotation/app/src/main/java/com/example/testopengl/util/LoggerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.testopengl.util;
-
-public class LoggerConfig {
-    public static final boolean ON = true;
-}
